75822f35140bd12b756c6190229b65e7022c6389
Dmitry Morozov <9984398@gmail.com>
2022-11-27 14:09:48 +0300

    DingleNumberIII: Done Tests : Passes

---
+++ HomeWorks/Week_1/LEETCODE/src/SingleNumberIII.cpp
@@ -0,0 +1,38 @@
+//
+// Created by Dmitry Morozov on 27/11/22.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+
+        int xor_result = 0;
+        for (auto num : nums) {
+            xor_result ^= num;
+        }
+
+        int mask = 1;
+        while ((xor_result & mask) == 0) {
+            mask <<= 1;
+        }
+
+        int first = 0;
+        int second = 0;
+        for (auto num : nums) {
+            if ((num & mask) == 0) {
+                first ^= num;
+            } else {
+                second ^= num;
+            }
+        }
+
+        return {first, second};
+
+    }
+
+};
