a9210111801c97304819977de3eaec90ad777532
Dmitry Morozov <9984398@gmail.com>
2022-12-15 13:06:49 +0300

    Week_3
    Fixed Array Partition

--- HomeWorks/Week_2/LEETCODE/src/ArrayPartition.cpp
+++ HomeWorks/Week_2/LEETCODE/src/ArrayPartition.cpp
@@ -3,6 +3,7 @@
 //
 
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -11,9 +12,9 @@ class Solution {
 public:
     int arrayPairSum(vector<int> &nums) {
 
-        sort(nums.begin(), nums.end());
+        std::sort(nums.begin(), nums.end());
 
-        int sum =  0;
+        int sum = 0;
 
         for (int i = 0; i < nums.size(); i += 2) {
 
