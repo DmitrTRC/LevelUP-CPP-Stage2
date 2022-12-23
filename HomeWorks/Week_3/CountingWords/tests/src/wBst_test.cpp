a536e3ba5136e723e2e3b12c60d815dafabbe986
Dmitry Morozov <9984398@gmail.com>
2022-12-23 20:41:15 +0300

    Week 3 :
    Bst: TESTS : DONE : PASSING

--- HomeWorks/Week_3/CountingWords/tests/src/wBst_test.cpp
+++ HomeWorks/Week_3/CountingWords/tests/src/wBst_test.cpp
@@ -34,3 +34,51 @@ TEST(wBST, Add) {
     EXPECT_EQ(tree.size(), 2);
 
 }
+
+TEST (wBST, Search) {
+
+    wBST tree;
+
+    wString word(L"word");
+
+    tree.Add(word);
+
+    EXPECT_EQ(tree.Search(word), 1);
+
+    wString word2(L"word2");
+
+    tree.Add(word2);
+
+    EXPECT_EQ(tree.Search(word2), 1);
+
+    wString word3(L"word3");
+
+    EXPECT_EQ(tree.Search(word3), 0);
+
+
+}
+
+TEST (wBST, size) {
+
+    wBST tree;
+
+    wString word(L"word");
+
+    tree.Add(word);
+
+    EXPECT_EQ(tree.size(), 1);
+
+    wString word2(L"word2");
+
+    tree.Add(word2);
+
+    EXPECT_EQ(tree.size(), 2);
+
+    wString word3(L"word3");
+
+    tree.Add(word3);
+
+    EXPECT_EQ(tree.size(), 3);
+
+
+}
