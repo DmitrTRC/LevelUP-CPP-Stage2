a1966f8c14437abbc71534d493fdab2c6ed53643
Dmitry Morozov <9984398@gmail.com>
2022-12-25 18:57:07 +0300

    Week 3 :
    Time measure : Tests : Passing

---
+++ HomeWorks/Week_3/CountingWords/tests/src/time_measure_test.cpp
@@ -0,0 +1,24 @@
+//
+// Created by Dmitry Morozov on 25/12/22.
+//
+
+#include "gtest/gtest.h"
+
+#include "T_Measure.hpp"
+
+#include <functional>
+
+
+TEST(TMeasure, Measure_Time) {
+
+    std::function<int(int)> fibonacci = [&fibonacci](int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    };
+
+    auto dt = measure_time(fibonacci, 40);
+
+    EXPECT_GT(dt, 0);
+}
+
